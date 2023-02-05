c17dd3b3136c2f095e41ac31b9c0ba6c56a89b9e
t3k <anoop.k.nair@gmail.com>
2023-02-04 22:01:12 -0600

    create apt packages, add pyenv

--- envy.sh
+++ envy.sh
@@ -1,20 +1,11 @@
 echo 'update system' 
 sudo apt-get update && sudo apt-get upgrade
+echo 'installing packages in package.txt' 
+xargs sudo apt-get install -y <packages.txt
 
-echo 'installing curl' 
-sudo apt install curl -y
-
-echo 'installing git' 
-sudo apt install git-all -y
-
-echo 'installing vim'
-sudo apt install vim -y
-
-echo 'installing wget unzip'
-sudo apt-get install wget unzip -y
-
-echo 'installing build-essential checkinstall libssl-dev'
-sudo apt install build-essential checkinstall libssl-dev -y
+echo 'installing pyenv'
+curl https://pyenv.run | bash
+pyenv install -v 3.11.1
 
 echo 'installing github cli'
 sudo apt install gh
